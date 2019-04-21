2e42fa0a437142d59d3c526b95b71db09f3d96f2
BomberBeetle <36681076+BomberBeetle@users.noreply.github.com>
2019-04-21 17:16:15 -0300

    switched to using hitherdither to approximate/dither the colors

--- drawPixelTest.py
+++ drawPixelTest.py
@@ -3,9 +3,15 @@ import sys
 import keyboard
 import mouse 
 import time
-from threading import Thread
+from hitherdither import palette 
+from hitherdither import ordered 
 from PIL import Image
 
+bayerMap =  [
+            [0,35,15],
+            [30,25,10],
+            [20,5,40],
+            ]
 #dictionary "ColorName": (xPos, yPos, (redVal, greenVal, blueVal))
 
 colors = {
@@ -20,6 +26,11 @@ colors = {
     "brown": (320,760,(172,116,68)),
     "gray": (425,760,(122,119,154)),
     }
+
+hexPalette = [
+        0x000000, 0xFFFFFF, 0xF94F4F, 0x63F94F, 0x46ACCF, 0xFFCF63, 0x85F3C6, 0xFFE5C2, 0xAC7444, 0x7A779A
+            ]
+
 #The degree of difference between two RGB Values in the form of (Red, Green Blue)
 def rgbDiff(a,b):
     rmean = (a[0] + b[0])//2
@@ -28,41 +39,19 @@ def rgbDiff(a,b):
     b = a[2] - b[2]
     return math.sqrt((((512+rmean)*r*r)>>8) + 4*g*g + (((767-rmean)*b*b)>>8))
 
-#key of the last color used to draw something
-lastColor = None
-
-
-resolutionMultiplier = float(input("please inform the resolution multiplier (default is 54x54, recommended is multiplier is 2.)\n"))
-
-#size of the picture drawn. larger pictures are very slow to draw.
-size = (math.floor(54*resolutionMultiplier), math.floor(54*resolutionMultiplier))
-
-
-unprosPic = Image.open(input("Please inform the name of the file (CaSe SENSitive!)\n"))
-
-pic = unprosPic.convert("RGB").resize(size, Image.ANTIALIAS)
-
-keyPic = []
-for x in range(0,math.floor(54*resolutionMultiplier)):
-    keyPic.insert(x,[])
-    for y in range(0,math.floor(54*resolutionMultiplier)):
-
-        lowestDiff = [sys.maxsize,""]
-        for key in colors.keys():
+def getMostSimilar(pixel, colors):
+    
+    lowestDiff = [sys.maxsize,""]
+    
+    for key in colors.keys():
 
             pixelDiff = rgbDiff(pic.getpixel((x,y)) ,colors[key][2])
             
             if pixelDiff < lowestDiff[0]:
                 
                 lowestDiff[0], lowestDiff[1] = pixelDiff, key
-                
-        pic.putpixel((x,y),colors[lowestDiff[1]][2])
-        keyPic[x].insert(y,lowestDiff[1])
-        
-                                                                                                                              
-pic.save("outpic.jpeg", "JPEG")
-input("IMAGE PROCESSING COMPLETE. DUMPING MODIFIED PICTURE TO 'outpic.jpeg'. AFTER SENDING ANY TEXT, YOU HAVE 30 SECONDS TO SWITCH TO A HAT IN TIME'S WINDOW. PRESS 'a' AT ANY TIME TO ABORT THE OPERATION.")
-test = ("ltblue", "red", "green", "blue","yellow","beige","brown","gray","white","black")
+
+    return lowestDiff
 
 def drawPixel(x,y,color, lastColor):
     if color != lastColor:
@@ -79,7 +68,37 @@ def drawPixel(x,y,color, lastColor):
     time.sleep(0.032)
     lastColor = color
     return lastColor
-###END OF DRAWPIXEL###
+
+#key of the last color used to draw something
+lastColor = None
+
+ditheringOrder = int(input("inform the order of dithering\n"))
+
+resolutionMultiplier = float(input("please inform the resolution multiplier (default is 54x54, recommended is multiplier is 2.)\n"))
+
+#size of the picture drawn. larger pictures are very slow to draw.
+size = (math.floor(54*resolutionMultiplier), math.floor(54*resolutionMultiplier))
+
+
+unprosPic = Image.open(input("Please inform the name of the file (CaSe SENSitive!)\n"))
+
+pic = unprosPic.convert("RGB").resize(size, Image.ANTIALIAS)
+
+pic = ordered.yliluoma.yliluomas_1_ordered_dithering(pic, palette.Palette(hexPalette), order=ditheringOrder).convert("RGB")
+
+
+keyPic = []
+                
+for x in range(0,math.floor(54*resolutionMultiplier)):
+    keyPic.insert(x,[])
+    for y in range(0,math.floor(54*resolutionMultiplier)):
+        mostSimilar = getMostSimilar(pic.getpixel((x,y)),colors)      
+        keyPic[x].insert(y,mostSimilar[1])
+                                                                                                                              
+pic.save("outpic.jpeg", "JPEG")
+input("IMAGE PROCESSING COMPLETE. DUMPING MODIFIED PICTURE TO 'outpic.jpeg'. AFTER SENDING ANY TEXT, YOU HAVE 30 SECONDS TO SWITCH TO A HAT IN TIME'S WINDOW. PRESS 'a' AT ANY TIME TO ABORT THE OPERATION.")
+test = ("ltblue", "red", "green", "blue","yellow","beige","brown","gray","white","black")
+
 
 time.sleep(30)
 for x in range(0, math.floor(54*resolutionMultiplier)):
