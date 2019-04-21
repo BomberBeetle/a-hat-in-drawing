2a26000ed690e5d2690c587c654ecc939920e457
BomberBeetle <36681076+BomberBeetle@users.noreply.github.com>
2019-04-21 18:06:10 -0300

    added suport for no dithering

--- drawPixelTest.py
+++ drawPixelTest.py
@@ -72,7 +72,8 @@ def drawPixel(x,y,color, lastColor):
 #key of the last color used to draw something
 lastColor = None
 
-ditheringOrder = int(input("inform the order of dithering\n"))
+ditheringOrder = 2**int(input("inform the level of dithering (0 is no dithering)\n"))
+dithering = (ditheringOrder != 1)
 
 resolutionMultiplier = float(input("please inform the resolution multiplier (default is 54x54, recommended is multiplier is 2.)\n"))
 
@@ -83,8 +84,9 @@ size = (math.floor(54*resolutionMultiplier), math.floor(54*resolutionMultiplier)
 unprosPic = Image.open(input("Please inform the name of the file (CaSe SENSitive!)\n"))
 
 pic = unprosPic.convert("RGB").resize(size, Image.ANTIALIAS)
-
-pic = ordered.yliluoma.yliluomas_1_ordered_dithering(pic, palette.Palette(hexPalette), order=ditheringOrder).convert("RGB")
+if dithering:
+    
+    pic = ordered.yliluoma.yliluomas_1_ordered_dithering(pic, palette.Palette(hexPalette), order=ditheringOrder).convert("RGB")
 
 
 keyPic = []
@@ -92,7 +94,9 @@ keyPic = []
 for x in range(0,math.floor(54*resolutionMultiplier)):
     keyPic.insert(x,[])
     for y in range(0,math.floor(54*resolutionMultiplier)):
-        mostSimilar = getMostSimilar(pic.getpixel((x,y)),colors)      
+        mostSimilar = getMostSimilar(pic.getpixel((x,y)),colors)
+        if not dithering:
+            pic.putpixel((x,y),colors[mostSimilar[1]][2])
         keyPic[x].insert(y,mostSimilar[1])
                                                                                                                               
 pic.save("outpic.jpeg", "JPEG")
